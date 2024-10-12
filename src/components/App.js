8f13610cce7cbebbe7ad9761b2b46a1ccf7f093a
Chris Horne <chrhorne@deloitte.com>
2024-10-12 10:04:55 -0700

    remove unused components

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,8 @@
-// src/components/App.js
 import React from 'react';
-// import { CssBaseline } from '@mui/material';
-// import { ThemeProvider } from '@mui/material/styles';
-// import theme from '../styles/theme';
-// import Layout from './Layout';
 import FDTable from './FDTable';
 
 function App() {
   return (
-    // <ThemeProvider theme={theme}>
-    //   <CssBaseline />
-    //   <Layout />
-    // </ThemeProvider>
     <div>
       <FDTable />
     </div>
