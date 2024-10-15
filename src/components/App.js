28b40db655c26fc471c0dcbcbb18b269f4c500cf
acouncil <acouncil@ny-mecm.com>
2024-10-14 23:34:26 -0700

    CSS/Columns

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,70 @@
-import React from 'react';
-import FDTable from './FDTable';
+import "./App.css";
+import * as React from "react";
+import { useTable } from "react-table";
 
 function App() {
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('http://localhost:3000/api/data')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "ID",
+        accessor: "id",
+      },
+      {
+        Header: "Insight Score",
+        accessor: "Insight Score",
+      },
+      {
+        Header: "Risk",
+        accessor: "Risk",
+      },
+      {
+        Header: "Outcomes",
+        accessor: "Outcomes",
+      },
+    ],
+    []
+  );
+
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
+
   return (
-    <div>
-      <FDTable />
+    <div className="App">
+      <div className="container">
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps()}>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => (
+                    <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                  ))}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
